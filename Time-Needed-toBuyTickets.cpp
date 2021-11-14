757c771d4e9ad57d54c445e6e4ba418fed56568f
Aroskar17 <93914017+Aroskar17@users.noreply.github.com>
2021-11-14 23:47:38 +0530

    Update Time-Needed-toBuyTickets.cpp

--- Time-Needed-toBuyTickets.cpp
+++ Time-Needed-toBuyTickets.cpp
@@ -17,7 +17,7 @@ Explanation:
 - In the second pass, everyone in the line buys a ticket and the line becomes [0, 1, 0].
 The person at position 2 has successfully bought 2 tickets and it took 3 + 3 = 6 seconds.
 */
-#include<bits/stdc++.h>
+
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
